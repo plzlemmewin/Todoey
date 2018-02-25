9cdb2f4ae191216f3c777600a22beb0d67f24c54
MAC <Mac@macs-imac.local>
2018-02-24 16:27:35 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by MAC on 2/24/18.
+//  Copyright © 2018 Jaime Lai. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
